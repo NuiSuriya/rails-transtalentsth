221df556f3126b953604509d5470ba645666540c
Nui Suriya <atchara.suriya@gmail.com>
2023-08-20 02:46:31 +0200

    Add AVO

---
+++ app/avo/resources/message_category_resource.rb
@@ -0,0 +1,13 @@
+class MessageCategoryResource < Avo::BaseResource
+  self.title = :name
+  self.includes = []
+  # self.search_query = -> do
+  #   scope.ransack(id_eq: params[:q], m: "or").result(distinct: false)
+  # end
+
+  field :id, as: :id
+  # Fields generated from the model
+  field :name, as: :text
+  field :contact_forms, as: :has_many
+  # add fields here
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount Avo::Engine, at: Avo.configuration.root_path
 
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -6,4 +7,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :contact_forms, only: [:new, :create]
+  resources :blog_posts, only: [:show]
 end
