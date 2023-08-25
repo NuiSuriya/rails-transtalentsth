46744014ded1a4a9dff8e720479941822a37a3fe
Nui Suriya <atchara.suriya@gmail.com>
2023-08-25 10:55:34 +0200

    fixed js bugs

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,3 +8,8 @@ pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
 pin "sweetalert2", to: "https://ga.jspm.io/npm:sweetalert2@11.7.23/dist/sweetalert2.all.js"
 pin "swiper/element/bundle", to: "https://ga.jspm.io/npm:swiper@10.2.0/swiper-element-bundle.mjs"
+pin "stimulus-carousel", to: "https://ga.jspm.io/npm:stimulus-carousel@5.0.1/dist/stimulus-carousel.mjs"
+pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:@hotwired/stimulus@3.2.2/dist/stimulus.js"
+pin "dom7", to: "https://ga.jspm.io/npm:dom7@4.0.6/dom7.esm.js"
+pin "ssr-window", to: "https://ga.jspm.io/npm:ssr-window@4.0.2/ssr-window.esm.js"
+pin "swiper/bundle", to: "https://ga.jspm.io/npm:swiper@8.4.7/swiper-bundle.esm.js"
