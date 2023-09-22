e717bb454e2a7af6dcc645b6dac67784869730d3
Nui Suriya <atchara.suriya@gmail.com>
2023-09-22 17:09:04 +0200

    bug fix

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,9 @@ module RailsTranstalentsth
       generate.test_framework :test_unit, fixture: false
     end
 
+    # set sunday as the beginning of the week
+    config.beginning_of_week = :sunday
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
