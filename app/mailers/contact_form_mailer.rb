74fcf8d98baa8628a1532973082a75d8588e0966
Nui Suriya <atchara.suriya@gmail.com>
2023-11-14 00:24:05 +0100

    change email setting

--- app/mailers/contact_form_mailer.rb
+++ app/mailers/contact_form_mailer.rb
@@ -1,6 +1,6 @@
 class ContactFormMailer < ApplicationMailer
   def contact_email(contact_form)
     @contact_form = contact_form
-    mail(reply_to: @contact_form.email ,to: "atchara.suriya@gmail.com", subject: "Message from #{@contact_form.name}")
+    mail(reply_to: @contact_form.email ,to: "nikki@transtalents.co", subject: "Message from #{@contact_form.name}")
   end
 end
